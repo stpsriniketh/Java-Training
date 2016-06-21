7b0117ed1230880650c87941d9e89c2366e3ff24
musheer.akbar <musheer.akbar@gmail.com>
2016-06-21 16:18:02 -0500

    file is updated to leap year

---
+++ main/com/java/training/LeapYear.java
@@ -0,0 +1,19 @@
+package com.java.training;
+
+import java.util.Scanner;
+
+public class LeapYear {
+	
+	public static void main(String [] args){
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter a year: ");
+		
+		int year = input.nextInt();
+		
+		boolean isLeapYear = (year % 4 == 0 && year % 100 !=0)|| (year % 400 == 0);
+		System.out.println( " Is " + year + " a **Leap Year** ? " + isLeapYear);
+		
+	}
+}
