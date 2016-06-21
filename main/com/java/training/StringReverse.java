c88a96bed27ea72addad3565f100783dcf4044ae
SRINIKETH S T P <SRINIKETH S T P>
2016-06-21 13:35:18 -0500

    STP: NEW JAVA TRAINING

---
+++ main/com/java/training/StringReverse.java
@@ -0,0 +1,13 @@
+package com.java.training;
+
+public class StringReverse 
+
+{
+	   public static void main(String[] args){
+	      String string="abcdef";
+	      String reverse = new StringBuffer(string).
+	      reverse().toString();
+	      System.out.println("\nString before reverse:"+string);
+	      System.out.println("String after reverse:"+reverse);
+	   }
+	}
